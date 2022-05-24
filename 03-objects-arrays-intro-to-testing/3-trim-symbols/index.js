f2b81dd0501f612f503db57d6c5af40c82e3b6a6
Syomkin Nikita Github <79851214062@yandex.ru>
2022-05-24 23:00:53 +0300

    3 module done

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,21 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (size === 0) {
+    return ''
+  }
 
+  if (!size) {
+    return string
+  }
+
+  const firstSlice = string.slice(0, size)
+  const rest = [...string.slice(size)]
+
+  return rest.reduce((acc, item) => {
+    if (!acc.endsWith(item.repeat(size))) {
+      acc += item
+    }
+    return acc
+  }, firstSlice)
 }

f8b23ea6841334e1aa6924d5396aaaa764c87788
Syomkin Nikita Github <79851214062@yandex.ru>
2022-05-24 18:59:42 +0300

    done invert-object

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,12 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-
+  if (obj) {
+    return Object.entries(obj).reduce((acc, item) => {
+      const [key, value] = item
+      acc[value] = key
+      return acc
+    }, {})
+  }
+  return
 }
